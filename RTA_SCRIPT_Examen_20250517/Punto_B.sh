c6478fac3cad4061764654eab572a9114ee7d25a
MFaii <faillamaximiliano@gmail.com>
2025-05-17 22:16:56 +0000

    MOD:Punto_B(sin terminar)

--- RTA_SCRIPT_Examen_20250517/Punto_B.sh
+++ RTA_SCRIPT_Examen_20250517/Punto_B.sh
@@ -47,3 +47,22 @@ sudo fdisk -l $DISCO
 
 read -p "Enter para continuar"
 
+#Formateo
+
+sudo fdisk $DISCO -l | grep Linux | awk '{print $1}' | xargs - I PARTICION sudo mkfs.ext4 PARTICION
+
+read -p "Enter para continuar: "
+echo "Add Montaje Persistente: "
+echo
+
+cat << EOF | sudo tee -a /etc/fstab
+
+EOF
+
+echo "monto todo"
+sudo mount -a
+
+echo "muestro lo que esta montado"
+echo 
+sudo df -h  | grep -E 'Filesystem|Examenes-UTN'
+
